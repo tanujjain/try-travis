c85a4c04aadf0450939834150b694c9e75cb528a
Tanuj Jain <tanuj.jain.10@gmail.com>
2019-04-09 18:55:07 +0200

    Added newline in test code.

--- tests/tests_all.py
+++ tests/tests_all.py
@@ -5,6 +5,7 @@ def test_addition():
     mycalculator = Calculator()
     assert mycalculator.myadd(3, 4) == 7
 
+
 def test_mutl():
     mycalculator = Calculator()
     assert mycalculator.mymult(3, 4) == 12
