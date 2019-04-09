d4b2d78b1b0e91eda2a4d60ed4a309679e7ac3ea
Tanuj Jain <tanuj.jain.10@gmail.com>
2019-04-09 18:51:04 +0200

    Added test for subtraction.

--- tests/tests_all.py
+++ tests/tests_all.py
@@ -4,3 +4,8 @@ from calculator import Calculator
 def test_addition():
     mycalculator = Calculator()
     assert mycalculator.myadd(3, 4) == 7
+
+
+def test_sub():
+    mycalculator = Calculator()
+    assert mycalculator.mysub(7, 4) == 3
